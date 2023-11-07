41039ebffcde669eb1b54c7997adfe83b6f6e435
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-07 15:20:51 +0000

    code to create books

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import BestBooks from "../components/BestBooks";
+import Form from "../components/Form"
 
 export default function Home({ books, setBooks }) {
 	return (
 		<main>
 			<h2>Home</h2>
-				<BestBooks books={books} setBooks={setBooks} />
+				<BestBooks books={books} setBooks={setBooks}/>
+				<Form books={books} setBooks={setBooks}/>
 		</main>
 	);
 }
