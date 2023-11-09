8debb77325d7a05fbca61b959523ac0e6bbc438b
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-09 16:46:47 +0000

    change host

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      // VERCEL domain="dev-cgd2lhokv774aknw.uk.auth0.com"
-      domain="dev-hcw0yxm63w57ez7b.uk.auth0.com"
-      // VERCEL clientId="FzRY5Sno68pfs8JgaoMt9gnZde1n80fu"
-      clientId="eV3xybo2tDnMtJS6zqCwvT263HtDIPOD"
+      domain="dev-cgd2lhokv774aknw.uk.auth0.com"
+      // domain="dev-hcw0yxm63w57ez7b.uk.auth0.com" (commented out are for local host)
+      clientId="FzRY5Sno68pfs8JgaoMt9gnZde1n80fu"
+      // clientId="eV3xybo2tDnMtJS6zqCwvT263HtDIPOD"
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
