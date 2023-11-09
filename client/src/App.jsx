4586f91de81e925d2382d84ee9b0fa7e2afa577a
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-09 16:43:02 +0000

    add authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,27 +5,35 @@ import About from "./pages/About";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Book from "./pages/Book";
 import Profile from "./pages/Profile";
+import LogInButton from "./components/LoginButton";
+import LogOutButton from "./components/LogOutButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import Welcome from "./components/Welcome";
 
 function App() {
-  const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState([]);
+	const { user, isAuthenticated, isLoading } = useAuth0();
 
-  return (
-    <BrowserRouter>
-      <header>
-        <h1>Our Books</h1>
-        <Link to="/"> Home</Link>
-        <Link to="/about"> About</Link>
-        <Link to="/profile"> Profile</Link>
-      </header>
+	return (
+		<BrowserRouter>
+			<header>
+				<h1>Our Books</h1>
 
-      <Routes>
-        <Route path="/" element={<Home books={books} setBooks={setBooks} />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/book/:id" element={<Book />} />
-        <Route path="/profile" elements={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-  );
+				{isAuthenticated ? <LogOutButton /> : <LogInButton />}
+
+				<Link to="/"> Home</Link>
+				<Link to="/about"> About</Link>
+				<Link to="/profile"> Profile</Link>
+			</header>
+
+			<Routes>
+				<Route path="/" element={<Home books={books} setBooks={setBooks} />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/book/:id" element={<Book />} />
+				<Route path="/profile" element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import BestBooks from "../components/BestBooks";
-import Form from "../components/Form"
+import Form from "../components/Form";
+import Welcome from "../components/Welcome";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home({ books, setBooks }) {
+	const { user, isAuthenticated, isLoading } = useAuth0();
+
 	return (
 		<>
-		<section>
-			<Form books={books} setBooks={setBooks}/>
+			<section>
+				<Form books={books} setBooks={setBooks} />
 			</section>
-		<main>
-				<BestBooks books={books} setBooks={setBooks}/>
-		</main>
+			<main>
+				{isAuthenticated ? (
+					<BestBooks books={books} setBooks={setBooks} />
+				) : (
+					<Welcome />
+				)}
+			</main>
 		</>
 	);
 }
